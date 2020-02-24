eb12d99eb5de57083858b12d33c6c06828981fd2
Kaustuvi <basu.kaustuvi@gmail.com>
2020-02-24 21:57:20 +0530

    fixed bug

--- equal_probability_circuit_util/parameter_optimizer.py
+++ equal_probability_circuit_util/parameter_optimizer.py
@@ -7,12 +7,12 @@ class ParameterOptimizer():
     def __init__(self):
         self.theta = 0
 
-    def cost_function(self, x00, x11, specialized_cost = False):
+    def cost_function(self, x00, x11, specialized_cost):
         if specialized_cost:
             return (x11 - 0.5)
         return (x00 - x11)**2
 
-    def optimize_circuit_parameter(self, qc, executable, shots, verbose = False, specialized_cost = False):
+    def optimize_circuit_parameter(self, qc, executable, shots, specialized_cost, verbose = False):
         learning_rate = 0.1
         precision = 0
         previous_step_size = 1
@@ -24,12 +24,12 @@ class ParameterOptimizer():
             sum_bitstrings = np.sum(measurement_bitstrings, axis=1)
             probability00 = list(sum_bitstrings).count(0) / shots
             probability11 = list(sum_bitstrings).count(2) / shots
-            self.theta = self.theta - learning_rate*self.cost_function(probability00, probability11)
+            self.theta = self.theta - learning_rate*self.cost_function(probability00, probability11, specialized_cost)
             previous_step_size = abs(self.theta - previous_theta)
             iteration = iteration + 1
             factor = np.round(self.theta / np.pi, 2)
             if verbose and iteration % 20 == 0:
-                print("Iteration", iteration, "\nProbability of |00> :", probability00, "\nProbability of |11> :", probability11, "\nCost Value:", self.cost_function(probability00, probability11), "\nTheta Value:", factor, "pi")
+                print("Iteration", iteration, "\nProbability of |00> :", probability00, "\nProbability of |11> :", probability11, "\nCost Value:", self.cost_function(probability00, probability11, specialized_cost), "\nTheta Value:", factor, "pi")
 
         return factor, iteration
 
